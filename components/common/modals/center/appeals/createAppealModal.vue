ebb92781300ccfc24623fd0d3fb254be521d269e
Абылай <abylay@MacBook-Air-Abylaj.local>
2022-11-12 12:32:21 +0600

    uniq color list

---
+++ components/common/modals/center/appeals/createAppealModal.vue
@@ -0,0 +1,23 @@
+<template>
+  <modal name="create-appeal" :close-on-out-click="false" close-button @onHide="clear()">
+    <div class="create-appeal">
+
+    </div>
+  </modal>
+</template>
+
+<script>
+export default {
+  name: "createAppealModal",
+  methods: {
+    // Очистить
+    clear() {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
